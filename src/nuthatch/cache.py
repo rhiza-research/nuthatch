68c64ad539df48db9a40f53efd8a09773af41da6
Joshua Adkins <josh@rhizaresearch.org>
2025-08-25 16:50:46 -0700

    first attempt at a cache backend

--- src/nuthatch/cache.py
+++ src/nuthatch/cache.py
@@ -1,21 +1,35 @@
-from deltalake import DeltaTable, write_deltalake
-import fsspec
+from deltalake import DeltaTable, write_deltalake, QueryBuilder
+from pathlib import Path
 import datetime
 from .backend import get_backend_by_name
 from .config import get_config
+import pyarrow as pa
+import pandas as pd
 
 class Cache():
     config_parameters=['filesystem', 'filesystem_options']
-    backend_name = ['metadata']
 
     def __init__(self, config, cache_key, namespace, args, location, requested_backend, backend_kwargs):
         self.cache_key = cache_key
         self.config = config
         self.namespace = namespace
-        self.fs = fsspec.core.url_to_fs(self.config['filesystem'], **self.config['filesystem_options'])[0]
+
+        base_path = Path(self.config['filesystem'])
+        self.table_path = base_path.joinpath('nuthatch_metadata.delta')
 
         # Instantiate the metadata store here so that _get_backend_from_metadata() works
+        if not DeltaTable.is_deltatable(self.table_path, storage_options=self.config['filesystem_options']):
+            DeltaTable.create(self.table_path,
+                              schema=pa.schema(
+                                [pa.field("cache_key", pa.string()), pa.field("backend", pa.string()),
+                                 pa.field("namespace", pa.string()), pa.field("state", pa.string()),
+                                 pa.field("last_modified", pa.timestamp()), pa.field("commit_hash", pa.string()),
+                                 pa.field("user", pa.string()), pa.field("path", pa.string())]
+                              ),
+                              storage_options=self.config['filesystem_options'],
+                              partion_by="cache_key")
 
+        self.dt = DeltaTable(self.table_path, storage_options=self.config['filesystem_options'])
 
         self.backend = None
         backend_class = None
@@ -28,30 +42,70 @@ class Cache():
             backend_config = get_config(location=location, requested_parameters=backend_class.config_parameters,
                                         backend_name=backend_class.backend_name)
             self.backend = backend_class(backend_config, cache_key, namespace, args, backend_kwargs)
+            self.backend_name = backend_class.backend_name
 
     def is_null(self):
-        pass
+        null_rows = QueryBuilder().register('metadata', self.dt).execute(f"""select * from metadata where cache_key = {self.cache_key}
+                                                                         AND namespace = {self.namespace}
+                                                                         AND state = 'null'""").read_all()
+        if len(null_rows) > 0:
+            return True
 
     def set_null(self):
-        pass
+        self._update_metadata_state(state='null')
 
     def delete_null(self):
-        pass
+        # Deleting a null is really just deleting a metadata
+        self._delete_metadata(null=True)
 
-    def _delete_metadata(self):
-        pass
+    def _delete_metadata(self, null=False):
+        if null:
+            self.dt.delete(predicate="cache_key = {self.cache_key} AND namespace = {self.namespace} AND state = 'null'")
+        else:
+            if self.backend_name:
+                self.dt.delete("cache_key = {self.cache_key} AND namespace = {self.namespace} AND backend = {self.backend_name}")
+            else:
+                raise RuntimeError("Can only delete non-null metadata with a valid backend")
 
     def _metadata_exists(self):
-        pass
+        rows = QueryBuilder().register('metadata', self.dt).execute(f"""select backend from metadata where cache_key = {self.cache_key}
+                                                                                AND namespace = {self.namespace}
+                                                                                AND backend = {self.backend_name}""").read_all()
+
+        if len(rows) > 0:
+            return True
+        else:
+            return False
 
     def _get_backend_from_metadata(self):
-        pass
+        backend_rows = QueryBuilder().register('metadata', self.dt).execute(f"""select backend from metadata where cache_key = {self.cache_key}
+                                                                                AND namespace = {self.namespace}""").read_all()
+        if len(backend_rows) == 0:
+            return None
+        else:
+            return backend_rows[0]
 
     def _set_metadata_pending(self):
-        pass
+        self._update_metadata_state(state='pending')
 
     def _commit_metadata(self):
-        pass
+        self._update_metadata_state(state='confirmed')
+
+    def _update_metadata_state(self, state=None):
+        if self._metadata_exists():
+            if state == 'null':
+                self.dt.update(predicate="cache_key = {self.cache_key} AND namespace = {self.namespace}",
+                               update={'state': 'null', 'last_modified': datetime.datetime.now()})
+            else:
+                self.dt.update(predicate="cache_key = {self.cache_key} AND namespace = {self.namespace} AND backend = {self.backend_name}",
+                               update={'state': state, 'last_modified': datetime.datetime.now()})
+        else:
+            df = pd.DataFrame({'cache_key': self.cache_key,
+                               'namespace': self.namespace,
+                               'backend': self.backend_name,
+                               'state': state,
+                               'last_modified': datetime.datetime.now()})
+            write_deltalake(self.dt, df, mode='append')
 
     def get_backend(self):
         return self.backend.backend_name
@@ -72,7 +126,10 @@ class Cache():
 
     def write(self, ds, upsert, primary_keys):
         if self.backend:
-            return self.backend.write(ds, upsert, primary_keys)
+            self._set_metadata_pending()
+            data = self.backend.write(ds, upsert, primary_keys)
+            self._commit_metadata()
+            return data
         else:
             raise RuntimeError("Cannot not write to an uninitialized backend")
 
