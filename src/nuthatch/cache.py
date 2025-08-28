5df0aa5b7d6f848ee1cf6989cc891e89d0277d86
Joshua Adkins <josh@rhizaresearch.org>
2025-08-28 13:25:24 -0700

    make it so that a cache can use sql

--- src/nuthatch/cache.py
+++ src/nuthatch/cache.py
@@ -8,9 +8,11 @@ from .backend import get_backend_by_name
 from .config import get_config
 import pyarrow as pa
 import pandas as pd
+import sqlalchemy
 
 class Cache():
-    config_parameters=['filesystem', 'filesystem_options']
+    database_parameters = ["driver", "host", "port", "database", "username", "password"]
+    config_parameters = ['filesystem', 'filesystem_options', 'metadata_location'] + database_parameters
     delta_tables = {}
     delta_table_configs = {}
 
@@ -22,35 +24,66 @@ class Cache():
         self.args = args
         self. backend_kwargs = backend_kwargs
 
-        base_path = self.config['filesystem']
-        self.table_path = join(base_path, 'nuthatch_metadata.delta')
+        # Either instantiate a delta table or a postgres table
+        if (('metadata_location' in config and config['metadata_location'] == 'filesystem') or
+            ('metadata_location' not in config and 'filesystem' in config) or
+            (any(param not in config for param in self.__class__.database_parameters))):
+            # This is a delta/filesystem type
+            base_path = self.config['filesystem']
+            table_path = join(base_path, 'nuthatch_metadata.delta')
 
-        options = None
-        if 'filesystem_options' in self.config:
-            options = self.config['filesystem_options']
-            for key, value in options.items():
-                options[key] = str(value)
+            options = None
+            if 'filesystem_options' in self.config:
+                options = self.config['filesystem_options']
+                for key, value in options.items():
+                    options[key] = str(value)
 
-        if (backend_location in self.__class__.delta_tables and
-            self.__class__.delta_table_configs[backend_location] == config):
-            self.dt = self.__class__.delta_tables[backend_location]
+            self.store = 'delta'
+            if (backend_location in self.__class__.delta_tables and
+                self.__class__.delta_table_configs[backend_location] == config):
+                self.dt = self.__class__.delta_tables[backend_location]
+            else:
+                # Instantiate the metadata store here so that _get_backend_from_metadata() works
+                if not DeltaTable.is_deltatable(table_path, storage_options=options):
+                    print("Instantiating empty delta table.")
+                    DeltaTable.create(table_path,
+                                      schema=pa.schema(
+                                        [pa.field("cache_key", pa.string()), pa.field("backend", pa.string()),
+                                         pa.field("namespace", pa.string()), pa.field("state", pa.string()),
+                                         pa.field("last_modified", pa.int64()), pa.field("commit_hash", pa.string()),
+                                         pa.field("user", pa.string()), pa.field("path", pa.string())]
+                                      ),
+                                      storage_options=options,
+                                      partition_by="cache_key")
+
+                self.dt = DeltaTable(table_path, storage_options=options)
+                self.__class__.delta_tables[backend_location] = self.dt
+                self.__class__.delta_table_configs[backend_location] = config
         else:
-            # Instantiate the metadata store here so that _get_backend_from_metadata() works
-            if not DeltaTable.is_deltatable(self.table_path, storage_options=options):
-                print("Instantiating empty delta table.")
-                DeltaTable.create(self.table_path,
-                                  schema=pa.schema(
-                                    [pa.field("cache_key", pa.string()), pa.field("backend", pa.string()),
-                                     pa.field("namespace", pa.string()), pa.field("state", pa.string()),
-                                     pa.field("last_modified", pa.int64()), pa.field("commit_hash", pa.string()),
-                                     pa.field("user", pa.string()), pa.field("path", pa.string())]
-                                  ),
-                                  storage_options=options,
-                                  partition_by="cache_key")
+            # This is a database type
+            database_url = sqlalchemy.URL.create(self.config['driver'],
+                                                 username = self.config['username'],
+                                                 password = self.config['password'],
+                                                 host = self.config['host'],
+                                                 port = self.config['port'],
+                                                 database = self.config['database'])
+            self.engine = sqlalchemy.create_engine(database_url)
+            metadata = sqlalchemy.MetaData()
 
-            self.dt = DeltaTable(self.table_path, storage_options=options)
-            self.__class__.delta_tables[backend_location] = self.dt
-            self.__class__.delta_table_configs[backend_location] = config
+            self.db_table = sqlalchemy.Table(
+                'nuthatch_metadata', metadata,
+                sqlalchemy.Column('cache_key', sqlalchemy.String),
+                sqlalchemy.Column('backend', sqlalchemy.String),
+                sqlalchemy.Column('namespace', sqlalchemy.String),
+                sqlalchemy.Column('state', sqlalchemy.String),
+                sqlalchemy.Column('last_modified', sqlalchemy.BigInteger),
+                sqlalchemy.Column('commit_hash', sqlalchemy.String),
+                sqlalchemy.Column('user', sqlalchemy.String),
+                sqlalchemy.Column('path', sqlalchemy.String)
+            )
+
+            metadata.create_all(self.engine, checkfirst=True)
+            self.store = 'database'
 
         self.backend = None
         self.backend_name = None
@@ -68,15 +101,80 @@ class Cache():
                 self.backend = backend_class(backend_config, cache_key, namespace, args, copy.deepcopy(backend_kwargs))
                 self.backend_name = backend_class.backend_name
 
-    def is_null(self):
+    def _delta_check_exists(self, state=None, include_backend=False):
+        base = f"""select * from metadata where cache_key = '{self.cache_key}' AND namespace = '{self.namespace}'"""
+        if include_backend:
+            base += f" AND backend = '{self.backend_name}'"
+        if state:
+            base += f" AND state = '{state}'"
 
-        null_rows = QueryBuilder().register('metadata', self.dt).execute(f"""select * from metadata where cache_key = '{self.cache_key}'
-                                                                         AND namespace = '{self.namespace}'
-                                                                         AND state = 'null'""").read_all()
-        if len(null_rows) > 0:
+        rows = QueryBuilder().register('metadata', self.dt).execute(base).read_all()
+
+        if len(rows) > 0:
             return True
         else:
             return False
+
+    def _sql_check_exists(self, state=None, include_backend=False):
+        statement = sqlalchemy.select(sqlalchemy.func.count(self.db_table.c.cache_key)).where(self.db_table.c.cache_key == self.cache_key)\
+                                                                    .where(self.db_table.c.namespace == self.namespace)
+        if state:
+            statement = statement.where(self.db_table.c.state == state)
+        if include_backend:
+            statement = statement.where(self.db_table.c.backend == self.backend_name)
+
+        with self.engine.connect() as conn:
+            num = conn.execute(statement)
+            if num.fetchone()[0] > 0:
+                return True
+            else:
+                return False
+
+    def _check_row_exists(self, state=None, include_backend=False):
+        if self.store == 'delta':
+            return self._delta_check_exists(state, include_backend)
+        else:
+            return self._sql_check_exists(state, include_backend)
+
+    def _sql_get_row(self, select, include_backend=False):
+        statement = sqlalchemy.select(select).where(self.db_table.c.cache_key == self.cache_key)\
+                                             .where(self.db_table.c.namespace == self.namespace)
+        if include_backend:
+            statement = statement.where(self.db_table.c.backend == self.backend_name)
+
+        with self.engine.connect() as conn:
+            rows = conn.execute(statement)
+            if rows.rowcount == 0:
+                return None
+            else:
+                return rows.fetchone()[0]
+
+    def _delta_get_row(self, select, include_backend=False):
+        base = f"""select {select} from metadata where cache_key = '{self.cache_key}' AND namespace = '{self.namespace}'"""
+        if include_backend:
+            base += f" AND backend = '{self.backend_name}'"
+
+        rows = QueryBuilder().register('metadata', self.dt).execute(base).read_all()
+
+        if len(rows) == 0:
+            return None
+        else:
+            return rows.to_batches()[0][0][0].as_py()
+
+    def _get_row(self, select, include_backend=False):
+        if self.store == 'delta':
+            return self._delta_get_row(select, include_backend)
+        else:
+            obj = None
+            if select == 'backend':
+                obj = self.db_table.c.backend
+            elif select == 'last_modified':
+                obj = self.db_table.c.last_modified
+
+            return self._sql_get_row(obj, include_backend)
+
+    def is_null(self):
+        return self._check_row_exists(state='null', include_backend=False)
 
     def set_null(self):
         self._update_metadata_state(state='null')
@@ -86,57 +184,40 @@ class Cache():
         self._delete_metadata(null=True)
 
     def _delete_metadata(self, null=False):
-        if null:
-            self.dt.delete(predicate=f"cache_key = '{self.cache_key}' AND namespace = '{self.namespace}' AND state = 'null'")
-        else:
-            if self.backend_name:
-                self.dt.delete(f"cache_key = '{self.cache_key}' AND namespace = '{self.namespace}' AND backend = '{self.backend_name}'")
+        if self.store == 'delta':
+            if null:
+                self.dt.delete(predicate=f"cache_key = '{self.cache_key}' AND namespace = '{self.namespace}' AND state = 'null'")
             else:
-                raise RuntimeError("Can only delete non-null metadata with a valid backend")
+                if self.backend_name:
+                    self.dt.delete(predicate=f"cache_key = '{self.cache_key}' AND namespace = '{self.namespace}' AND backend = '{self.backend_name}'")
+                else:
+                    raise RuntimeError("Can only delete non-null metadata with a valid backend")
+        else:
+            statement = sqlalchemy.delete(self.db_table).where(self.db_table.c.cache_key == self.cache_key)\
+                                                        .where(self.db_table.c.namespace == self.namespace)
+            if null:
+                statement = statement.where(self.db_table.c.state == 'null')
+            else:
+                statement = statement.where(self.db_table.c.backend == self.backend_name)
+            with self.engine.connect() as conn:
+                conn.execute(statement)
+                conn.commit()
+
 
     def _metadata_confirmed(self):
         if not self.backend:
             return False
 
-        rows = QueryBuilder().register('metadata', self.dt).execute(f"""select * from metadata where cache_key = '{self.cache_key}'
-                                                                                AND namespace = '{self.namespace}'
-                                                                                AND backend = '{self.backend_name}'
-                                                                                AND state = 'confirmed'""").read_all()
-
-        if len(rows) > 0:
-            return True
-        else:
-            return False
+        return self._check_row_exists(state='confirmed', include_backend=True)
 
     def _metadata_exists(self):
-        rows = QueryBuilder().register('metadata', self.dt).execute(f"""select backend from metadata where cache_key = '{self.cache_key}'
-                                                                                AND namespace = '{self.namespace}'
-                                                                                AND backend = '{self.backend_name}'""").read_all()
-
-        if len(rows) > 0:
-            return True
-        else:
-            return False
+        return self._check_row_exists(state=None, include_backend=True)
 
     def _get_backend_from_metadata(self):
-        backend_rows = QueryBuilder().register('metadata', self.dt).execute(f"""select backend from metadata where metadata.cache_key = '{self.cache_key}'
-                                                                                AND metadata.namespace = '{self.namespace}'""").read_all()
-
-        if len(backend_rows) == 0:
-            return None
-        else:
-            return backend_rows.to_batches()[0][0][0].as_py()
+        return self._get_row('backend', include_backend=False)
 
     def last_modified(self):
-        backend_rows = QueryBuilder().register('metadata', self.dt).execute(f"""select last_modified from metadata where metadata.cache_key = '{self.cache_key}'
-                                                                                AND metadata.namespace = '{self.namespace}'
-                                                                                AND metadata.backend = '{self.backend_name}'""").read_all()
-
-        if len(backend_rows) == 0:
-            return None
-        else:
-            return backend_rows.to_batches()[0][0][0].as_py()
-
+        return self._get_row('last_modified', include_backend=False)
 
     def _set_metadata_pending(self):
         self._update_metadata_state(state='pending')
@@ -150,26 +231,48 @@ class Cache():
             sha = repo.head.object.hexsha
         else:
             sha = 'no_git_repo'
-        if self._metadata_exists():
-            if state == 'null':
-                self.dt.update(predicate=f"cache_key = '{self.cache_key}' AND namespace = '{self.namespace}'",
-                               new_values={'state': 'null', 'last_modified': datetime.datetime.now(datetime.timezone.utc).timestamp() * 1000000,
-                                           'commit_hash': sha, 'user': getpass.getuser(), 'path': self.backend.get_file_path()})
+        if self.store == 'delta':
+            if self._metadata_exists():
+                values = {'state': state, 'last_modified': datetime.datetime.now(datetime.timezone.utc).timestamp() * 1000000,
+                          'commit_hash': sha, 'user': getpass.getuser(), 'path': self.backend.get_file_path()}
+                if state == 'null':
+                    self.dt.update(predicate=f"cache_key = '{self.cache_key}' AND namespace = '{self.namespace}'",
+                                   new_values = values)
+                else:
+                    self.dt.update(predicate=f"cache_key = '{self.cache_key}' AND namespace = '{self.namespace}' AND backend = '{self.backend_name}'",
+                                   new_values= values)
             else:
-                self.dt.update(predicate=f"cache_key = '{self.cache_key}' AND namespace = '{self.namespace}' AND backend = '{self.backend_name}'",
-                               new_values={'state': state, 'last_modified': datetime.datetime.now(datetime.timezone.utc).timestamp() * 1000000,
-                                           'commit_hash': sha, 'user': getpass.getuser(), 'path': self.backend.get_file_path()})
-        else:
-            df = pd.DataFrame({'cache_key': [self.cache_key],
-                               'namespace': [str(self.namespace)],
-                               'backend': [self.backend_name],
-                               'commit_hash': [sha],
-                               'user': [getpass.getuser()],
-                               'path' : [self.backend.get_file_path()],
-                               'state': [state],
-                               'last_modified': [datetime.datetime.now(datetime.timezone.utc).timestamp() * 1000000]})
+                df = pd.DataFrame({'cache_key': [self.cache_key],
+                                   'namespace': [str(self.namespace)],
+                                   'backend': [self.backend_name],
+                                   'commit_hash': [sha],
+                                   'user': [getpass.getuser()],
+                                   'path' : [self.backend.get_file_path()],
+                                   'state': [state],
+                                   'last_modified': [datetime.datetime.now(datetime.timezone.utc).timestamp() * 1000000]})
 
-            write_deltalake(self.dt, df, mode='append')
+                write_deltalake(self.dt, df, mode='append')
+        else:
+            if self._metadata_exists():
+                statement = sqlalchemy.update(self.db_table).where(self.db_table.c.cache_key == self.cache_key)\
+                                                            .where(self.db_table.c.namespace == self.namespace)
+
+                if state == 'null':
+                    statement = statement.where(self.db_table.c.backend == self.backend_name)
+
+                statement = statement.values(state=state, last_modified=datetime.datetime.now(datetime.timezone.utc).timestamp()*1000000,
+                                             commit_hash=sha, user=getpass.getuser(), path=self.backend.get_file_path())
+            else:
+                statement = sqlalchemy.insert(self.db_table).values(state=state,
+                                                                    last_modified=datetime.datetime.now(datetime.timezone.utc).timestamp()*1000000,
+                                                                    commit_hash=sha, user=getpass.getuser(), path=self.backend.get_file_path(),
+                                                                    backend=self.backend_name,
+                                                                    cache_key=self.cache_key,
+                                                                    namespace=self.namespace)
+
+            with self.engine.connect() as conn:
+                conn.execute(statement)
+                conn.commit()
 
     def get_backend(self):
         return self.backend.backend_name
---
+++ tests/test_db_metastore.py
@@ -0,0 +1,97 @@
+from nuthatch.cache import Cache
+
+from nuthatch.config import config_parameter
+from nuthatch.config import get_config
+from google.cloud import secretmanager
+
+@config_parameter('password', location='root')
+def postgres_write_password():
+    """Get a postgres write password."""
+    client = secretmanager.SecretManagerServiceClient()
+
+    response = client.access_secret_version(
+        request={"name": "projects/750045969992/secrets/sheerwater-postgres-write-password/versions/latest"})
+    key = response.payload.data.decode("UTF-8")
+
+    return key
+
+local_config = get_config(location='root', requested_parameters=Cache.config_parameters)
+
+
+def test_meta_init():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', None, None)
+    assert cache.db_table is not None
+
+def test_requested_backend():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', 'basic', None)
+    assert cache.get_backend() == 'basic'
+
+
+def test_write_exists():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', 'basic', None)
+
+    a = 5
+    cache.write(a)
+
+    assert cache.exists()
+
+def test_infer_backend():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', 'basic', None)
+
+    a = 5
+    cache.write(a)
+
+    cache = Cache(local_config, "test_key", "test", {}, 'root', None, None)
+    assert cache.get_backend() == 'basic'
+
+
+def test_write_read():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', 'basic', None)
+
+    a = 5
+    cache.write(a)
+
+    assert cache.exists()
+    data = cache.read()
+
+    assert data == a
+
+def test_overwrite():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', 'basic', None)
+
+    a = 5
+    cache.write(a)
+    data = cache.read()
+    assert data == a
+
+    a = 10
+    cache.write(a)
+    data = cache.read()
+    assert data == a
+
+
+def test_set_recall_null():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', 'basic', None)
+
+    cache.set_null()
+    assert cache.is_null()
+
+def test_delete():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', 'basic', None)
+
+    a = 5
+    cache.write(a)
+    data = cache.read()
+    assert data == a
+
+    cache.delete()
+    assert not cache.exists()
+
+def test_delete_null():
+    cache = Cache(local_config, "test_key", "test", {}, 'root', 'basic', None)
+
+    cache.set_null()
+    assert cache.is_null()
+
+    cache.delete_null()
+    assert not cache.is_null()
