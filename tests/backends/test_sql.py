a458e506d5627d296725f3a6994300c86cbcb8a0
Joshua Adkins <josh@rhizaresearch.org>
2025-12-03 18:31:14 -0800

    get test sql working

--- tests/backends/test_sql.py
+++ tests/backends/test_sql.py
@@ -1,4 +1,4 @@
-from tabular_test import multi_tab_test_pandas, multi_tab_test_dask
+from ..backends.tabular_test import multi_tab_test_pandas, multi_tab_test_dask
 from nuthatch.config import config_parameter
 from google.cloud import secretmanager
 
