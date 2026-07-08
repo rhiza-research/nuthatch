690a69f7d317e56e0b007c3eec5b2b8505bef331
Genevieve Flaspohler <geflaspohler@gmail.com>
2026-07-08 14:21:56 -0700

    Added testing

--- tests/backends/test_terracotta.py
+++ tests/backends/test_terracotta.py
@@ -59,6 +59,27 @@ def time_latlon_array():
     return ds
 
 
+@cache(cache_args=[])
+def time_latlon_array_with_null_chunks():
+    """Generate a timeseries dataset where some time slices are all null."""
+    lats = np.arange(-90, 90, 0.25)
+    lons = np.arange(-180, 180, 0.25)
+    times = pd.date_range("2001-01-01", "2001-01-03", freq='1d')
+
+    data_shape = (len(lats), len(lons), len(times))
+    empty_data = np.ones(data_shape) * np.random.randint(1000000)
+    empty_data[:, :, :2] = np.nan
+
+    ds = xr.Dataset(
+        data_vars= {
+            'data': (['lat', 'lon', 'time'], empty_data),
+        },
+        coords={"lat": lats, "lon": lons, "time": times},
+    )
+
+    return ds
+
+
 def test_terracotta():
     # cache the array
     ds = simple_latlon_array(backend='zarr')
@@ -73,3 +94,10 @@ def test_terracotta():
     ds = time_latlon_array(backend='zarr', storage_backend='terracotta', cache_mode='overwrite')
     ds = time_latlon_array(backend='terracotta')
     assert len(ds) == 5
+
+
+def test_terracotta_all_null_time_chunks():
+    time_latlon_array_with_null_chunks(backend='zarr')
+    time_latlon_array_with_null_chunks(backend='zarr', storage_backend='terracotta', cache_mode='overwrite')
+    ds = time_latlon_array_with_null_chunks(backend='terracotta')
+    assert len(ds) == 1
